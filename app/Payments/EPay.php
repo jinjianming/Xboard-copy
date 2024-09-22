90df86f4b72e3ccbe7b6f5d00725cf5911beb31b
jinjianming <57084209+jinjianming@users.noreply.github.com>
2024-09-22 23:32:37 +0800

    Update EPay.php

--- app/Payments/EPay.php
+++ app/Payments/EPay.php
@@ -37,7 +37,7 @@ class EPay
             'money' => $order['total_amount'] / 100,
             'name' => $order['trade_no'],
             'notify_url' => $order['notify_url'],
-            // 'return_url' => $order['return_url'],
+            'return_url' => $order['return_url'],
             // 'return_url' => $_SERVER['HTTP_REFERER'] . '#/dashboard/checkout/' . $order['trade_no'],
             ’return_url’ => $_SERVER[‘HTTP_REFERER’] . ‘#/order/’ . $order[‘trade_no’],
             'out_trade_no' => $order['trade_no'],
